93d8045fddee09f11a14982550fc8b015ceb4970
Gourav Sethi <61932647+gouravsethi933@users.noreply.github.com>
2022-02-22 12:58:20 +0530

    string

---
+++ src/components/States.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import City from "./City";
+
+function States({ states }) {
+  const [cityIndex, setCityIndex] = useState(-1);
+  const [townIndex, setTownIndex] = React.useState(-1);
+
+  const handleCity = (index) => {
+    if (cityIndex === index) {
+      setCityIndex(-1);
+      return;
+    }
+    setCityIndex(index);
+    setTownIndex(-1);
+  };
+
+  return (
+    <>
+      {states.map((ele, index) => {
+        return (
+          <>
+            <div id={`state${index + 1}`} onClick={() => handleCity(index)}>
+              {ele.name}
+            </div>
+          </>
+        );
+      })}
+      {cityIndex !== -1 && (
+        <City index={townIndex} cities={states[cityIndex].cities}></City>
+      )}
+    </>
+  );
+}
+
+export default States;
